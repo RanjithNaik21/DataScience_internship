4212ba06edc7ee03961041ae1ab5b9ef7d274ed6
RanjithNaik21 <139787232+RanjithNaik21@users.noreply.github.com>
2023-07-17 20:50:27 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,19 @@
+import sys 
+import os
+import unittest
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__)+'/src"))) 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__) + '/tests' )))
+
+#from care import get_answer from core import main
+
+from core if name ==_main_:
+
+import main
+
+f = open("packets/tcp.txt","r")
+
+B = open('packets/udp.txt', 'r')
+
+main(f)
